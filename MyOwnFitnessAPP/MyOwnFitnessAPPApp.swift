0835399be840d6e4d31dd4b29ac2735805348abf
kalayn <kalyan>
2026-03-16 08:26:58 -0500

    Initial Commit

---
+++ MyOwnFitnessAPP/MyOwnFitnessAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyOwnFitnessAPPApp.swift
+//  MyOwnFitnessAPP
+//
+//  Created by kalyan on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct MyOwnFitnessAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
