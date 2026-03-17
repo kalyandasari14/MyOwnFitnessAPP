94d8a72d6fe61dbeed87ed238861a404ea16a602
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-16 19:18:12 -0500

    builded a mealview

---
+++ MyOwnFitnessAPP/MealDataView.swift
@@ -0,0 +1,24 @@
+//
+//  MealDataView.swift
+//  MyOwnFitnessAPP
+//
+//  Created by kalyan on 3/16/26.
+//
+
+import SwiftUI
+import SwiftData
+
+struct MealDataView: View {
+    @Environment(\.modelContext) var context
+    @Environment(\.dismiss) var dismiss
+    
+    
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MealDataView()
+}
--- MyOwnFitnessAPP/MealView.swift
+++ MyOwnFitnessAPP/MealView.swift
@@ -6,16 +6,51 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct MealView: View {
+    @Query var meals : [Meal]
+    @Environment(\.modelContext) var context
+    @State private var showingMealView = false
     var body: some View {
-        Text("Meals")
-        Text("RedView")
-        Text("plainview")
-        Text("logview")
+        
+        Group{
+            if meals.isEmpty{
+                ContentUnavailableView("NO Meals", systemImage: "carrot", description: Text("Chop Chop Hurry Up!, You cannot build muscles without eating 😄"))
+            }else{
+                List{
+                    ForEach(meals){meal in
+                        VStack{
+                            Text(meal.name)
+                                .font(.title)
+                                .foregroundStyle(.primary)
+                            Text("\(meal.calories)")
+                                .font(.title)
+                                .foregroundStyle(.secondary)
+                        }
+                        
+                    }
+                }
+            }
+        }.navigationTitle("Add Meals")
+            .toolbar{
+                ToolbarItem{
+                    Button{
+                        showingMealView = true
+                    }label: {
+                        Image(systemName: "plus")
+                    }
+                }
+            }
+            .sheet(isPresented: $showingMealView){
+                MealDataView()
+            }
+        
     }
 }
 
 #Preview {
-    MealView()
+    NavigationStack{
+        MealView()
+        .modelContainer(for: Meal.self, inMemory: true)}
 }
