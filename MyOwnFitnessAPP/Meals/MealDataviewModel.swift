0fa2ab045fccbb0cd4cda53c4deb91960f0cc420
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-21 11:38:29 -0500

    Pulling an api from the meal information

---
+++ MyOwnFitnessAPP/Meals/MealDataviewModel.swift
@@ -0,0 +1,16 @@
+//
+//  MealDataviewModel.swift
+//  MyOwnFitnessAPP
+//
+//  Created by kalyan on 3/21/26.
+//
+
+import Foundation
+import Observation
+
+
+@Observable
+class MealDataviewModel{
+    
+    var meals : [FoodProducts]
+}
