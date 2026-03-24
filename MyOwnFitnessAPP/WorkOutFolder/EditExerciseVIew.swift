45a6599956b6c311f4b5a080ebd18d61390b7cbc
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-23 23:02:56 -0500

    Added editView Option for sets and weight

---
+++ MyOwnFitnessAPP/WorkOutFolder/EditExerciseVIew.swift
@@ -0,0 +1,18 @@
+//
+//  EditExerciseVIew.swift
+//  MyOwnFitnessAPP
+//
+//  Created by kalyan on 3/23/26.
+//
+
+import SwiftUI
+
+struct EditExerciseVIew: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditExerciseVIew()
+}
