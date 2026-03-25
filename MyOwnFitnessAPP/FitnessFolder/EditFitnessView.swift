539886439767a30e94eaa08ab3c9c3ecf29e82aa
kalyandasari14 <kalyandasari80@gmail.com>
2026-03-24 20:34:33 -0500

    added couple more edit views, so that we can have more control over data

---
+++ MyOwnFitnessAPP/FitnessFolder/EditFitnessView.swift
@@ -0,0 +1,18 @@
+//
+//  EditFitnessView.swift
+//  MyOwnFitnessAPP
+//
+//  Created by kalyan on 3/24/26.
+//
+
+import SwiftUI
+
+struct EditFitnessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    EditFitnessView()
+}
